908919f1506d82ab34daebdee8df210fd430fc4c
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-03-18 21:41:41 +0500

    Parsing has been successfully fixed

--- parsers/libparser.js
+++ parsers/libparser.js
@@ -1,8 +1,15 @@
+// optifine:OptiFine:1.8.8_HD_U_I7
+// net.minecraft:launchwrapper:1.12
 
 function libParser(libName) {
-    const pathSplit = libName.replaceAll(".", "/").replaceAll(":", "/");
-    const fullPath = pathSplit + pathSplit[pathSplit.length - 1] + ".jar";
-    console.log("[Debug] Parsed path: " + fullPath);
+    const groups = libName.split(":");
+    const group0 = groups[0].split(".");
+    const group1 = groups.slice(1, groups.length - 1);
+    const version = groups[groups.length - 1];
+    const artifact = groups[groups.length - 2];
+    const filename = artifact + "-" + version + ".jar";
+    const fullPath = group0.join("/") + "/" + group1.join("/") + "/" + filename;
+    console.log("[Debug] Generated path: " + fullPath);
 
     return fullPath;
 }
