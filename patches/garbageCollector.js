a168605e5e29ee4eacdd423aaaadab61c2bd01c3
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-03-27 19:27:45 +0500

    Delete patches directory

--- patches/garbageCollector.js
+++
@@ -1,6 +0,0 @@
-
-function getLauncherArg(string) {
-    return string += " -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:+UseStringDeduplication  -Xmx256m -Xms4096m -XX:+PerfDisableSharedMem -XX:+AlwaysPreTouch -XX:MaxGCPauseMillis=266 -XX:G1HeapRegionSize=16m";
-}
-
-module.exports = getLauncherArg;
