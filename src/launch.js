5b05b0ba905d0d1f26a30609630a03f82f2c666d
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-03-31 16:53:26 +0300

    Fixed LWJGL.dll bug.

--- src/launch.js
+++ src/launch.js
@@ -25,8 +25,9 @@ function launch(version, username, uuid = "a", javaP) {
                const minecraft = JSON.parse(fs.readFileSync("./minecraft/versions/" + version + "/" + version + ".json"));
                const libs = JSON.parse(fs.readFileSync("./minecraft/versions/" + version + "/" + version + ".json")).libraries;
                const minecraftArguments = minecraft.minecraftArguments ? minecraft.minecraftArguments : minecraft.arguments.game.filter(e => typeof e !== "object").join(" ");
-	let command = javaP + ' -D"org.lwjgl.librarypath"="' + resolve(nativesDir) + '" -D"java.library.path"="' + resolve(nativesDir) + '" -cp "' + (minecraft.inheritsFrom ? resolve("./minecraft/versions/" + minecraft.inheritsFrom + "/" + minecraft.inheritsFrom + ".jar;") : "") + resolve(clientJar) + ";" + libs.map(e => resolve("./minecraft/libraries/" + (e?.downloads?.artifact?.path || e?.downloads?.classifiers["natives-" + (process.platform == "win32" ? "windows" : (process.platform == "darwin" ? "macos" : "linux"))]?.path || parseLib(e?.downloads?.artifact?.name || e?.downloads?.classifiers?.name || e?.name))))
-		.join(";") + ";" + (minecraft.inheritsFrom ? (JSON.parse(fs.readFileSync("./minecraft/versions/" + minecraft.inheritsFrom + "/" + minecraft.inheritsFrom + ".json")).libraries.map(e => resolve("./minecraft/libraries/" + (e?.downloads?.artifact?.path || e?.downloads?.classifiers["natives-" + (process.platform == "win32" ? "windows" : (process.platform == "darwin" ? "macos" : "linux"))]?.path || parseLib(e?.downloads?.artifact?.name || e?.downloads?.classifiers?.name || e?.name))))).join(";") : "") + '" ' + mainClass + ' ' + minecraftArguments.replace("${auth_player_name}", username)
+               const classpath = (minecraft.inheritsFrom ? resolve("./minecraft/versions/" + minecraft.inheritsFrom + "/" + minecraft.inheritsFrom + ".jar;") : "") + resolve(clientJar) + ";" + libs.map(e => resolve("./minecraft/libraries/" + (e?.downloads?.artifact?.path || e?.downloads?.classifiers["natives-" + (process.platform == "win32" ? "windows" : (process.platform == "darwin" ? "macos" : "linux"))]?.path || parseLib(e?.downloads?.artifact?.name || e?.downloads?.classifiers?.name || e?.name))))
+		.join(";") + ";" + (minecraft.inheritsFrom ? (JSON.parse(fs.readFileSync("./minecraft/versions/" + minecraft.inheritsFrom + "/" + minecraft.inheritsFrom + ".json")).libraries.map(e => resolve("./minecraft/libraries/" + (e?.downloads?.artifact?.path || e?.downloads?.classifiers["natives-" + (process.platform == "win32" ? "windows" : (process.platform == "darwin" ? "macos" : "linux"))]?.path || parseLib(e?.downloads?.artifact?.name || e?.downloads?.classifiers?.name || e?.name))))).join(";") : "");
+	let command = javaP + ' -D"org.lwjgl.librarypath"="' + resolve(nativesDir) + '" -D"java.library.path"="' + resolve(nativesDir) + '" -cp "' + classpath + '" ' + mainClass + ' ' + minecraftArguments.replace("${auth_player_name}", username)
 		.replace("${version_name}", version)
 		.replace("${game_directory}", resolve("./minecraft"))
 		.replace("${assets_root}", '"' + resolve(assetsDir.split("/")
@@ -36,7 +37,9 @@ function launch(version, username, uuid = "a", javaP) {
 		.replace("${auth_uuid}", uuid)
 		.replace("--accessToken ${auth_access_token}", "")
 		.replace("${user_properties}", "normal")
-		.replace("${user_type}", "full")
+		.replace("${classpath}", classpath)
+                              .replace("${user_type}", "full")
+                              .replace("${natives_directory}", resolve(nativesDir))
                               .replace("${version_type}", "EthyrMC") + " --accessToken letmeplaylol";
 	console.log("[Ethyr] Command generated: " + patchGCMem(command));
 
