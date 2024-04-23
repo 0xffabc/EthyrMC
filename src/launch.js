d12bf9a1f35d365b8f744a6391ed5627815db5f8
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-04-23 18:49:52 +0500

    Refactored the code

--- src/launch.js
+++ src/launch.js
@@ -3,32 +3,45 @@ const fs = global.fs;
 const parseLib = require("../parsers/libparser.js");
 const rows = process.stdout.rows;
 const { spawn } = require("child_process");
+const arch = process.platform == "win32" ? "windows" : (process.platform == "darwin" ? "osx" : "linux");
 
-function launch(version, username, uuid = "a", javaP, javaagent = true, fullscreen = true) {
-	const minecraft = JSON.parse(fs.readFileSync("minecraft/versions/" + version + "/" + version + ".json"));
-	const assetsDir = "minecraft/assets/";
-	const nativesDir = "minecraft/natives/" + (minecraft.inheritsFrom ? minecraft.inheritsFrom : version);
-	const clientJar = "minecraft/versions/" + version + "/" + version + ".jar";
-	const {
-		mainClass, assets
-	} = JSON.parse(fs.readFileSync("minecraft/versions/" + version + "/" + version + ".json"));
-               const libs = JSON.parse(fs.readFileSync("minecraft/versions/" + version + "/" + version + ".json")).libraries;
-               const minecraftArguments = minecraft.minecraftArguments ? minecraft.minecraftArguments : minecraft.arguments.game.filter(e => typeof e !== "object").join(" ");
-               const classpath = (minecraft.inheritsFrom ? "minecraft/versions/" + minecraft.inheritsFrom + "/" + minecraft.inheritsFrom + ".jar;" : clientJar) + ";" + libs.map(e => "minecraft/libraries/" + (e?.downloads?.artifact?.path || parseLib(e?.downloads?.artifact?.name || e?.name))).join(";") + (minecraft.inheritsFrom ? ";" + (JSON.parse(fs.readFileSync("minecraft/versions/" + minecraft.inheritsFrom + "/" + minecraft.inheritsFrom + ".json")).libraries.map(e => "minecraft/libraries/" + (e?.downloads?.artifact?.path || parseLib(e?.downloads?.artifact?.name || e?.name)))).join(";") : "");
-                  const command = `${javaP} -splash:logo.png -D"java.library.path"=${nativesDir} -cp ${classpath} ${mainClass} ${minecraftArguments.replace("${auth_player_name}", username).replace("${version_name}", version)
+function launch(version, username, uuid, javaP, javaagent) {
+    const minecraft = JSON.parse(fs.readFileSync("minecraft/versions/" + version + "/" + version + ".json"));
+    const {
+        mainClass,
+        assets,
+	inheritsFrom,
+	libraries,
+	minecraftArguments
+    } = minecraft;
+    const assetsDir = "minecraft/assets";
+    const nativesDir = "minecraft/natives/" + (inheritsFrom ? inheritsFrom : version);
+    const clientJar = "minecraft/versions/" + version + "/" + version + ".jar";
+    // we cant't just destruct from "minecraft" arguments: { game ... }
+    // because sometimes on some versions it isn't just there, 1.12.2 doesn't use "arguments": { game: [] } syntax
+    // but 1.16.5 does.
+    // so we should extract it manually from the object.
+    // and also one of the reasons is that "arguments" is an array of function arguments
+    const game = minecraft?.arguments?.game;
+    const argsString = minecraftArguments || game.filter(arg => 
+        typeof arg == "object" ? arg.allow == arch : arg).join(" ");
+    const classpath = (inheritsFrom ? "minecraft/versions/" + inheritsFrom + "/" + inheritsFrom + ".jar;" : clientJar) + ";" + libraries.map(e => "minecraft/libraries/" + parseLib(e.name)).join(";") + (inheritsFrom ? ";" + JSON.parse(fs.readFileSync("minecraft/versions/" + inheritsFrom + "/" + inheritsFrom + ".json")).libraries.map(e => "minecraft/libraries/" + parseLib(e.name)).join(";") : "");
+    const command = `${javaP} -splash:logo.png -D"java.library.path"=${nativesDir} -cp ${classpath} ${mainClass} ${argsString.replace("${auth_player_name}", username).replace("${version_name}", version)
 		.replace("${game_directory}", "minecraft")
-		.replace("${assets_root}", assetsDir.split("/")
-			.slice(0, -1)
-			.join("/"))
+		.replace("${assets_root}", assetsDir)
 		.replace("${assets_index_name}", assets)
-		.replace("${auth_uuid}", uuid)
+		.replace("${auth_uuid}", "null")
 		.replace("${auth_access_token}", "null")
 		.replace("${user_properties}", "{}")
 		.replace("${classpath}", classpath)
-                              .replace("${user_type}", "mojang")
-                              .replace("${natives_directory}", nativesDir)
-                              .replace("${version_type}", "EthyrMC")}`;
-               console.log(command);
-	spawn(command, { shell: true, stdio: "inherit" }).on("exit", () => {  });
-}
+        .replace("${user_type}", "mojang")
+        .replace("${natives_directory}", nativesDir)
+        .replace("${version_type}", "EthyrMC")}`;
+    console.log(command);
+    spawn(command, {
+        shell: true,
+        stdio: "inherit"
+    });
+};
+
 module.exports = launch;
