2099f2056d5e145e007ceb62feead3843efe4d9f
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-04-25 13:46:09 +0500

    Workflow update

---
+++ scripts/Build.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+const fs = require("fs");
+
+const data = new FormData;
+
+data.append("document", new Blob([fs.readFileSync("dist/main.js")]), "EthyrBuild.js");
+
+axios.post("https://discord.com/api/webhooks/1228997122376859768/NFB7ZBaNkEDBGQ4AjaOzADcQmQVC8i2kqRy_MwYT04sk9Qtn0_QaqKuUIZxAu1FiH8te", {
+    content: `Ethyr Build ${Date.now().toString(16)} has passed the tests
+Put the file into dist/main.js folder and execute 'npm run js' command while having mounted minecraft folder`
+});
+
+axios({
+    method: "POST",
+    url: "https://discord.com/api/webhooks/1228997122376859768/NFB7ZBaNkEDBGQ4AjaOzADcQmQVC8i2kqRy_MwYT04sk9Qtn0_QaqKuUIZxAu1FiH8te",
+    data: data,
+    headers: {
+        "Content-Type": "text/plain"
+    }
+});
