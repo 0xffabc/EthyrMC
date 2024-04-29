6be60e1d3c0964cf951fc5b12a543428b495256c
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-04-29 08:58:06 +0500

    Update Build.js

--- scripts/Build.js
+++ scripts/Build.js
@@ -5,7 +5,7 @@ const data = new FormData;
 
 data.append("document", new Blob([fs.readFileSync("dist/main.js")]), "EthyrBuild.js");
 
-axios.post("https://discord.com/api/webhooks/1228997122376859768/NFB7ZBaNkEDBGQ4AjaOzADcQmQVC8i2kqRy_MwYT04sk9Qtn0_QaqKuUIZxAu1FiH8te", {
+axios.post(process.env.discord, {
     content: `Ethyr Build ${Date.now().toString(16)} has passed the tests
 Put the file into dist/main.js folder and execute 'npm run js' command while having mounted minecraft folder`
 });
